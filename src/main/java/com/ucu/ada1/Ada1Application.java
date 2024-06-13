6bbc30695d268c6fdee5e2fb7532991488597e68
Diego Alexis Silveira Cabrera <diego.silveira@mercadolibre.com>
2024-06-13 19:30:28 -0300

    feat() initial scaffolding

---
+++ src/main/java/com/ucu/ada1/Ada1Application.java
@@ -0,0 +1,13 @@
+package com.ucu.ada1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ada1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ada1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucu/ada1/Ada1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucu.ada1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ada1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
