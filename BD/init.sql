52361095cebde28b7f978df9fc0664aaba084f47
FranciscoNasso <francisco.nasso@correo.ucu.edu.uy>
2024-06-18 19:13:00 -0300

    Docker pronto con sql

--- BD/init.sql
+++ BD/init.sql
@@ -1,26 +1,27 @@
-CREATE DATABASE IF NOT EXIST "Olimpiadas";
+CREATE DATABASE IF NOT EXISTS Olimpiadas;
+USE Olimpiadas;
 
 CREATE TABLE Persona (
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50),
     correo VARCHAR(50),
-    fecha_nacimiento DATE,
-)
+    fecha_nacimiento DATE
+);
 
 CREATE TABLE Categoria (
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     sexo VARCHAR(50),
-    cant_max_atletas INT,
-)
+    cant_max_atletas INT
+);
 
 CREATE TABLE Deporte (
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     categoria_id INT,
-    FOREIGN KEY (categoria_id) REFERENCES categoria(id),
-)
+    FOREIGN KEY (categoria_id) REFERENCES Categoria(id)
+);
 
 
 CREATE TABLE Atleta (
@@ -28,13 +29,13 @@ CREATE TABLE Atleta (
     deporte_participa INT,
     categoria_participa INT,
     FOREIGN KEY (id) REFERENCES Persona(id),
-    FOREIGN KEY (deporte_participa) REFERENCES deporte(id),
-    FOREIGN KEY (categoria_participa) REFERENCES categoria(id),
-)
+    FOREIGN KEY (deporte_participa) REFERENCES Deporte(id),
+    FOREIGN KEY (categoria_participa) REFERENCES Categoria(id)
+);
 
 CREATE TABLE Juez (
     id INT PRIMARY KEY,
     deporte_juzga INT,
     FOREIGN KEY (id) REFERENCES Persona(id),
-    FOREIGN KEY (deporte_juzga) REFERENCES deporte(id),
-)
+    FOREIGN KEY (deporte_juzga) REFERENCES Deporte(id)
+);
